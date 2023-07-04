4affe8afd396ac4979dbfbf98b5da083b8c6afaf
lulabath <lunaagustinabath@gmail.com>
2023-07-03 22:09:50 -0300

    empezando conjs

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ const hideImgAside = () => {
     asideImg.classList.add('hidden');
     asideTexto.classList.remove('hidden');
 }
+
+//URL IMAGEN// 
+const inputUrl = document.getElementById('input-url');
+const memeImg = document.getElementById('meme-img');
+
+inputUrl.addEventListener('input', (e)=> changeBackground(e))
+
+const changeBackground = (e) => {
+    console.log(e.target.value)
+    memeImg.style.backgroundImage = `url('${e.target.value}')`
+}
+
+//INPUT RANGE//
+const inputBrillo = document.getElementById('range-brillo');
+const inputOpacidad = document.getElementById('range-opacidad');
+const inputContraste = document.getElementById('range-contraste');
+const inputDesenfoque = document.getElementById('range-desenfoque');
+const inputGrises = document.getElementById('range-grises');
+const inputSepia = document.getElementById('range-sepia');
+const inputHue = document.getElementById('range-hue');
+const inputSaturado = document.getElementById('range-saturado');
+const inputNegativo = document.getElementById('range-negativo');
+
+
+const filtros = (e) =>{
+    console.log(e.target.value)
+    memeImg.style.filter = `brightness(${e.target.value})`
+}
+
+inputBrillo.addEventListener('input', (e)=> filtros(e));
+inputContraste.addEventListener('input', (e)=> filtros(e));
